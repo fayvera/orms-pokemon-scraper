1d171649c4aaf942895e0ef3ce6a2e4182c000bb
Fay Vera <fvera.cavalcante@gmail.com>
2021-02-09 16:14:17 -0500

    commit tree baa73c011b71664b463b058fffe4f4ce67b880cd

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
-class Pokemon
+require 'pry'
+class Pokemon   
+    attr_accessor :id, :name, :type, :db
+
+ def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+ end
+ def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+ end
+
+ def self.find(id_num, db)
+    pkmn = db.execute("SELECT * FROM pokemon WHERE id = ?", id_num).flatten
+    Pokemon.new(id: pkmn[0], name: pkmn[1], type: pkmn[2], db: db)
+ end
+
+
 end
